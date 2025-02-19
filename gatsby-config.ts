85f64f7ffe3cea254502f5e826ee5328754aec2c
Javi Cardenas <javicard@pm.me>
2025-02-19 18:05:33 -0500

    added google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,27 +3,51 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `javicard.com`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://javicard.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-DVMEV7VN1R"],
+        gtagConfig: {
+          anonymize_ip: true, // Google Analytics will not store the full IP address, making users slightly more anonymous
+          cookie_expires: 0, // Users will not be recognized across multiple visits unless they log in or re-identify themselves
+        },
+        pluginConfig: {
+          head: true, // Places script in <head> for better performance
+          respectDNT: true, // Respects "Do Not Track" browser setting
+          exclude: ["/preview/**", "/do-not-track/me/too/"], // Exclude specific pages or URL patterns from being tracked
+          delayOnRouteUpdate: 0, // No delay in tracking page views
+        },
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
 
 export default config;
