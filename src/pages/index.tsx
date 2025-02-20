2249c7d19909211ba745a0172a631a89c825eaba
Javi Cardenas <javicard@pm.me>
2025-02-19 19:52:06 -0500

    testing formatter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ import * as React from "react";
 const IndexPage = (): JSX.Element => {
   return (
     <main>
+      
+      
+      
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
