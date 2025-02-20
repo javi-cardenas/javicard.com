471cda142ea5b402d3cda37128c03cf3f6a9fc60
Javi Cardenas <javicard@pm.me>
2025-02-20 06:50:42 -0500

    update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,23 @@
 import * as React from "react";
 import { Link, HeadFC, PageProps } from "gatsby";
 
-const pageStyles = {
+const pageStyles: React.CSSProperties = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 };
-const headingStyles = {
+
+const headingStyles: React.CSSProperties = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
 };
 
-const paragraphStyles = {
+const paragraphStyles: React.CSSProperties = {
   marginBottom: 48,
 };
-const codeStyles = {
+
+const codeStyles: React.CSSProperties = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
@@ -26,9 +28,9 @@ const codeStyles = {
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
+      <h1 style={headingStyles}>Page Not Found</h1>
       <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
+        Sorry 😔, we could not find what you were looking for.
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
@@ -38,7 +40,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
           </>
         ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        Go to the <Link to="/">home page</Link>.
       </p>
     </main>
   );
