c5c59204d9423857247b061847885cb43595e1b0
Javi Cardenas <jcard-dev@pm.me>
2025-05-14 20:00:21 -0400

    first deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,8 +17,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  // organizationName: 'javi-cardenas', // Usually your GitHub org/user name.
-  // projectName: 'javicard.com', // Usually your repo name.
+  organizationName: 'javi-cardenas', // Usually your GitHub org/user name.
+  projectName: 'javicard.com', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
